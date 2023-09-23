4cf3dd36a7953258d95f4975082916189a82ce0d
Leo <pericolileonardo@gmail.com>
2023-09-23 20:02:28 +0200

    editSlice Added

--- frontend/src/slices/ordersSlice.js
+++ frontend/src/slices/ordersSlice.js
@@ -17,7 +17,32 @@ export const ordersFetch = createAsyncThunk("orders/ordersFetch", async () => {
     }
 });
 
-export const ordersEdit = createAsyncThunk();
+export const ordersEdit = createAsyncThunk(
+    "orders/ordersEdit",
+    async (values, { getState }) => {
+        const state = getState();
+
+        let currentOrder = state.orders.list.filter(
+            (order) => order._id === values.id
+        );
+
+        const newOrder = {
+            ...currentOrder[0],
+            delivery_status: values.delivery_status,
+        };
+
+        try {
+            const response = await axios.put(
+                `${url}/orders/${values.id}`,
+                newOrder,
+                setHeaders()
+            );
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+);
 
 const ordersSlice = createSlice({
     name: "orders",
@@ -32,6 +57,19 @@ const ordersSlice = createSlice({
             state.status = "success";
         },
         [ordersFetch.rejected]: (state, action) => {
+            state.status = "rejected";
+        },
+        [ordersEdit.pending]: (state, action) => {
+            state.status = "pending";
+        },
+        [ordersEdit.fulfilled]: (state, action) => {
+            const updatedOrders = state.list.map((order) => 
+            order._id === action.payload._id ? action.payload : order
+            );
+            state.list = updatedOrders;
+            state.status = "success";
+        },
+        [ordersEdit.rejected]: (state, action) => {
             state.status = "rejected";
         },
     }
