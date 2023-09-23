d4d4cc8f5ee886dacbc5839b817f8d1155515075
Leo <pericolileonardo@gmail.com>
2023-09-23 20:01:55 +0200

    orderList done

--- frontend/src/components/admin/list/OrdersList.jsx
+++ frontend/src/components/admin/list/OrdersList.jsx
@@ -3,12 +3,10 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { productsDelete } from "../../../slices/productsSlice";
-import { ordersFetch } from "../../../slices/ordersSlice";
-
+import { ordersEdit, ordersFetch } from "../../../slices/ordersSlice";
+import moment from "moment";
 
 export default function OrdersList() {
-
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { list } = useSelector((state) => state.orders);
@@ -18,53 +16,88 @@ export default function OrdersList() {
         dispatch(ordersFetch());
     }, [dispatch]);
  
-    const rows = list && list.map((item) => {
+    const rows = list && list.map((order) => {
         return {
-            id: item._id,
-            imageUrl: item.image?.url,
-            pName: item.name,
-            pDesc: item.desc,
-            price: item.price?.toLocaleString(),
+            id: order._id,
+            cName: order.shipping.name,
+            pName: (order.total / 100)?.toLocaleString(),
+            dStatus: order.delivery_status,
+            date: moment(order.createdAt).fromNow(),
             };
         });
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 220 },
-        { field: 'imageUrl', headerName: 'Image', width: 80, 
+      { field: "id", headerName: "ID", width: 220 },
+      {
+        field: "cName",
+        headerName: "Name",
+        width: 120,
+      },
+      {
+        field: "pName",
+        headerName: "Amount $",
+        width: 100,
+      },
+      {
+        field: "dStatus",
+        headerName: "Status",
+        width: 100,
         renderCell: (params) => {
-            return
-            <></>
-                ;
-            },
+          return (
+            <div>
+              {params.row.dStatus === "pending" ? (
+                <Pending>Pending</Pending>
+              ) : params.row.dStatus === "dispatched" ? (
+                <Dispatched>Dispatched</Dispatched>
+              ) : params.row.dStatus === "delivered" ? (
+                <Delivered>Delivered</Delivered>
+              ) : (
+                "error"
+              )}
+            </div>
+          );
         },
-        { field: 'pName', headerName: 'Name', width: 130 },
-        {
-        field: 'pDesc',
-        headerName: 'Description',
-        width: 130,
-        },
-        {
-        field: 'price',
-        headerName: 'Price',
-        width: 80,
-        },
-        {
-        field: 'actions',
-        headerName: 'Actions',
+      },
+      {
+        field: "date",
+        headerName: "Date",
+        width: 120,
+      },
+      {
+        field: "actions",
+        headerName: "Actions",
         sortable: false,
-        width: 170,
+        width: 220,
         renderCell: (params) => {
-            return
-            <></>
-               ; 
-
-            },
+          return (
+            <Actions>
+              <DispatchBtn onClick={() => handleOrderDispatch(params.row.id)}>Dispatch</DispatchBtn>
+              <DeliveryBtn onClick={() => handleOrderDeliver(params.row.id)}>Delivery</DeliveryBtn>
+              <View onClick={() => navigate(`/order/${params.row.id}`)}>View</View>
+            </Actions>
+          );
         },
+      },
     ];
 
-    const handleDelete = (id) =>{
-        dispatch(productsDelete(id))
-    }
+    const handleOrderDispatch = (id) => {
+        dispatch(
+            ordersEdit({
+                id,
+                delivery_status: "dispatched",
+
+            })
+        );
+    };
+    const handleOrderDeliver = (id) => {
+        dispatch(
+            ordersEdit({
+                id,
+                delivery_status: "delivered",
+
+            })
+        );
+    };
 
   return (
     <div style={{ height: 400, width: '100%' }}>
@@ -80,6 +113,19 @@ export default function OrdersList() {
   );
 }
 
+const Actions = styled.div`
+    width:100%;
+    display: flex;
+    justify-content: space-between;
+    button {
+        border: none;
+        outline: none;
+        padding: 3px 5px;
+        color: white;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+`;
 
 const DispatchBtn = styled.button`
     background-color: rgb(38, 198, 249);
